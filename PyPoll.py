c182c144924308affb4ba1ab00938a8f75b6abd4
martinez.jose279@gmail.com <martinez.jose279@gmail.com>
2021-03-28 18:15:08 -0400

    Added code to working python file.

--- PyPoll.py
+++ PyPoll.py
@@ -10,6 +10,12 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Initalizing vote counter variable
 total_votes = 0
 
+# Initializing the candidate options list
+candidate_options = []
+
+#Initializing an empty dictionary for candidate votes
+candidate_votes = {}
+
 # Opening the election results and reading the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -19,6 +25,19 @@ with open(file_to_load) as election_data:
 
     # Printing each row in the CSV file
     for row in file_reader:
+        # Adding to the total vote count
         total_votes += 1
-    
-print(total_votes)
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing cadidate add that candidate to the list of candidates
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        
+        candidate_votes[candidate_name] += 1
+
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes) / float(total_votes) * 100
+    print(f"{candidate_name}: received {vote_percentage}% of the vote.")
