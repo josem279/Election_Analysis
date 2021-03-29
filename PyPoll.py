4922d836da0b3ec736294c102e095871896f52c6
martinez.jose279@gmail.com <martinez.jose279@gmail.com>
2021-03-29 10:45:33 -0400

    Added code to python file

--- PyPoll.py
+++ PyPoll.py
@@ -9,9 +9,8 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Initalizing vote counter variable
 total_votes = 0
-# Initializing the candidate options list
+# Initializing the candidate options list and candiate votes dictionary
 candidate_options = []
-#Initializing an empty dictionary for candidate votes
 candidate_votes = {}
 
 # Winning Candidate and Winning Count Tracker
@@ -22,7 +21,6 @@ winning_percentage = 0
 # Opening the election results and reading the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-
     # Reading the header row
     headers = next(file_reader)
 
@@ -35,30 +33,48 @@ with open(file_to_load) as election_data:
 
         # If the candidate does not match any existing cadidate add that candidate to the list of candidates
         if candidate_name not in candidate_options:
+            # Add the candidate to the candidate list
             candidate_options.append(candidate_name)
+            # And begin tracking that candidate's vote count
             candidate_votes[candidate_name] = 0
-        
+        # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-# Iterate through the candidate list.
-for candidate_name in candidate_votes:
-    # Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
-    # Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
+# Save the results to our text file
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal
+    election_results = (
+        f"\nElection Results \n"
+        f"----------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"----------------------\n")
+    print (election_results, end= "")
+    # Save the final vote count to the text file
+    txt_file.write(election_results)
+    
+    # Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # Print each candidate, their vote count, and percentage to the terminal
+        candidate_results = print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Determine if the votes is greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-         # If true then set winning_count = votes and winning_percent = vote_percentage.
-         winning_count = votes
-         winning_percentage = vote_percentage
-         # And, set the winning_candidate equal to the candidate's name.
-         winning_candidate = candidate_name
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent = vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
 
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
+    #Print the winning candidates' resutls to the terminal
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+
+# print(winning_candidate_summary)
